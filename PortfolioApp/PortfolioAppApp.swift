1140e2b4b27f98744a5bc6bf0e1ee4ce10415338
mich balkany <askmitchb@gmail.com>
2023-12-11 15:37:12 -0500

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Mich balkany on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
